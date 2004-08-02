cfb85a22497504a97e363e90afd31eae817a82f5
Andre Gosselin <Andre.Gosselin@dfo-mpo.gc.ca>
2004-08-02 14:45:56 +0000

    Initial revision

---
+++ pyhdf/setup.py
@@ -0,0 +1,33 @@
+# $Id: setup.py,v 1.1 2004-08-02 14:45:56 gosselin Exp $
+# $Log: not supported by cvs2svn $
+
+# distutils setup file for the pyhdf package
+
+from distutils.core import setup, Extension
+
+# SD API
+_sdext =    Extension('pyhdf._sdext', 
+                       sources   = ["pyhdf/sdext_wrap.c"],
+		       #include_dirs=["non standard path where hdf includes live"],
+		       #library_dirs=["non standard path where hdf libs live"],
+		       #extra_objects=["extra stuff passed to the linker"],
+		       libraries = ["mfhdf", "df", "jpeg", "z"])
+
+setup(name         = 'pyhdf',
+      author       = 'Andre Gosselin',
+      author_email = 'gosselina@dfo-mpo.gc.ca',
+      description  = 'Python interface to the NCSA HDF4 library',
+      keywords     = ['hdf', 'netcdf', 'Numeric', 'python'],
+      license      = 'public',
+      long_description = 'The pyhdf package wraps the functionality '
+                         'of the NCSA HDF4 library inside a Python OOP '
+			 'framework. Only the SD API is implemented for '
+			 'now, but other APIs will be covered in the '
+			 'near future. SD variables are read/written '
+			 'through Numeric arrays. netCDF files can also '
+			 'be read and modified with pyhdf.',
+      url          = 'ftp://nordet.qc.dfo-mpo.gc.ca/pub/soft/pyhdf',
+      version      ='0.5-1',
+      packages     = ['pyhdf'],
+      ext_modules  = [_sdext]
+      )
