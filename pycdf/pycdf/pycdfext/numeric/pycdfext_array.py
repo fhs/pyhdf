9db80e03e65ef04d49f416cb154e08bf84cc3124
Andre Gosselin <Andre.Gosselin@dfo-mpo.gc.ca>
2006-01-02 23:41:58 +0000

    This commit was manufactured by cvs2svn to create tag 'pycdf_0_6-0'.

--- pycdf/pycdf/pycdfext/numeric/pycdfext_array.py
+++
@@ -1,9 +0,0 @@
-# $Id: pycdfext_array.py,v 1.1 2006-01-02 18:51:15 gosselin_a Exp $
-# $Name: not supported by cvs2svn $
-# $Log: not supported by cvs2svn $
-
-# Those imports are dependent on the 'Numeric' package.
-# They will be imported in turn by 'pycdf.py'.
-
-_ARRAYPKG = "Numeric"
-from Numeric import array
--- pyhdf/pyhdf/__init__.py
+++
@@ -1,3 +0,0 @@
-# $Id: __init__.py,v 1.3 2004-08-02 15:22:59 gosselin Exp $
-# $Log : $
-
