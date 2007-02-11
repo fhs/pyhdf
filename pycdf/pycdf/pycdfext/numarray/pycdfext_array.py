0a3e7fc37d39b25726d2b068f5e7781976d8e2f2
Andre Gosselin <Andre.Gosselin@dfo-mpo.gc.ca>
2007-02-11 22:09:06 +0000

    Cleanup the references.

--- pycdf/pycdf/pycdfext/numarray/pycdfext_array.py
+++ pycdf/pycdf/pycdfext/numarray/pycdfext_array.py
@@ -1,10 +1,19 @@
-# $Id: pycdfext_array.py,v 1.1 2006-01-02 18:51:15 gosselin_a Exp $
+# $Id: pycdfext_array.py,v 1.2 2007-02-11 22:09:06 gosselin_a Exp $
 # $Name: not supported by cvs2svn $
 # $Log: not supported by cvs2svn $
+# Revision 1.1  2006/01/02 18:51:15  gosselin_a
+# The 'pycdf' directory has been restructured so as to allow
+# installing pycdf with either the Numeric or numarray array packages.
+# The directory top level now holds the package-independent
+# python code. 'numeric' and 'numarray' directories hold the
+# package-dependent parts.
+#
 
 # Those imports are dependent on the 'numarray' package.
 # They will be imported in turn by 'pycdf.py'.
 
-_ARRAYPKG = "numarray"
+__all__ = ["_ARRAYPKGNAME", "array", "_arrayPkg"]
 
+_ARRAYPKGNAME = "numarray"
 from numarray import array
+import numarray as _arrayPkg
