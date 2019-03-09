76b9ed5a62e5557d7c6881884a1cb241ad8dae2c
Fazlul Shahriar <fshahriar@gmail.com>
2019-03-08 22:58:57 -0500

    Update copyright year in docs

--- doc/conf.py
+++ doc/conf.py
@@ -47,7 +47,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'pyhdf'
-copyright = u'2014, pyhdf authors'
+copyright = u'2019, pyhdf authors'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
