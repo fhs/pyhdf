11c862bed893354db741f20cf1121e8bcacaab00
Fazlul Shahriar <fshahriar@gmail.com>
2019-03-14 12:17:36 -0400

    Test on macOS in Travis CI

---
+++ .travis/install_osx_miniconda.bash
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# We do this conditionally because it saves us some downloading if the
+# version is the same.
+if [[ "$TRAVIS_PYTHON_VERSION" == "2.7" ]]; then
+	curl https://repo.anaconda.com/miniconda/Miniconda2-latest-MacOSX-x86_64.sh > miniconda.sh;
+else
+	curl https://repo.anaconda.com/miniconda/Miniconda3-latest-MacOSX-x86_64.sh > miniconda.sh;
+fi
+bash miniconda.sh -b -p $HOME/miniconda
+export PATH="$HOME/miniconda/bin:$PATH"
+hash -r
+conda config --set always_yes yes --set changeps1 no
+conda update -q conda
+# Useful for debugging any issues with conda
+conda info -a
+
+conda create -q -n test-environment python=$TRAVIS_PYTHON_VERSION numpy hdf4 nose
+source activate test-environment
+
+export LIBRARY_DIRS=$CONDA_PREFIX/Library/bin:$CONDA_PREFIX/Library/lib
+export INCLUDE_DIRS=$CONDA_PREFIX/Library/include
