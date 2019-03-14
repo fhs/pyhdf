a4cd1323697daf6a47808faf13cb330d554272e9
Fazlul Shahriar <fshahriar@gmail.com>
2019-03-14 12:38:51 -0400

    Try to fix travis

--- .travis/install_osx_miniconda.bash
+++ .travis/install_osx_miniconda.bash
@@ -2,7 +2,7 @@
 
 # We do this conditionally because it saves us some downloading if the
 # version is the same.
-if [[ "$TRAVIS_PYTHON_VERSION" == "2.7" ]]; then
+if [[ "$PYHDF_PYTHON_VERSION" == "2.7" ]]; then
 	curl https://repo.anaconda.com/miniconda/Miniconda2-latest-MacOSX-x86_64.sh > miniconda.sh;
 else
 	curl https://repo.anaconda.com/miniconda/Miniconda3-latest-MacOSX-x86_64.sh > miniconda.sh;
@@ -15,8 +15,8 @@ conda update -q conda
 # Useful for debugging any issues with conda
 conda info -a
 
-conda create -q -n test-environment python=$TRAVIS_PYTHON_VERSION numpy hdf4 nose
+conda create -q -n test-environment python=$PYHDF_PYTHON_VERSION numpy hdf4 nose
 source activate test-environment
 
-export LIBRARY_DIRS=$CONDA_PREFIX/Library/bin:$CONDA_PREFIX/Library/lib
-export INCLUDE_DIRS=$CONDA_PREFIX/Library/include
+export LIBRARY_DIRS=$CONDA_PREFIX/lib
+export INCLUDE_DIRS=$CONDA_PREFIX/include
